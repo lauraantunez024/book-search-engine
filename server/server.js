0bf4670ae1e56bf76b578ca81f1eae75f183b7d4
Laura Antunez <lauraantunez024@gmail.com>
2022-07-31 19:23:53 -0400

    fully functioning in development but need to fix login

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
-const {authMiddleware} = require('./utils/auth')
+const { authMiddleware } = require('./utils/auth')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
